58194cf26c6b2cee2bd1abbf71cdef498282bbb8
Skye Im <fluffyterran@gmail.com>
2026-07-29 21:32:44 +0200

    RSS: Poll every hour

--- src/rss_poller/constants.rs
+++ src/rss_poller/constants.rs
@@ -5,7 +5,7 @@ const MINUTE_S: i64 = 60;
 const HOUR_S: i64 = 3600;
 
 /// The duration between scanning a given RSS feed
-pub static POLL_INTERVAL: Duration = Duration::new(HOUR_S * 12, 0).unwrap();
+pub static POLL_INTERVAL: Duration = Duration::new(HOUR_S * 1, 0).unwrap();
 
 /// The duration between making requests to WorldAnvil
 pub static REQUEST_INTERVAL_STD: StdDuration = StdDuration::new(5 * MINUTE_S as u64, 0);
